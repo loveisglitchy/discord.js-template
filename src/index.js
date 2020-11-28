d8a01416e132289268cbc934d71bf141ec27cdfb
loveisglitchy <72410854+loveisglitchy@users.noreply.github.com>
2020-11-28 08:06:35 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-// require the our additons to the Discord.js Client Class.
-const MDClient = require('./Structures/MDClient');
-// get out token, prefix and owners.
-const config = require('../config.json');
+require("dotenv").config();
 
-// creates a new instance of the Client.
-const client = new MDClient(config);
-// login to the websocket, load the events and commands.
+const Client = require('./Structures/Client');
+
+const config = process.env;
+
+
+const client = new Client(config);
+
+client.init();
 client.start();
