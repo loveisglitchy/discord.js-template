08ed6400fecfa19b8f55795621905bd495aa01c3
loveisglitchy <72410854+loveisglitchy@users.noreply.github.com>
2020-11-28 08:12:42 -0600

    Add files via upload

---
+++ src/Structures/mongoose.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+module.exports.init = (client, message) => {
+    
+    const dbOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: true,
+        poolSize: 5,
+        connectTimeoutMS: 10000,
+        family: 4,
+    }
+
+    mongoose.connect(process.env.connect, dbOptions)
+    mongoose.set('useFindAndModify', false);
+    mongoose.Promise = global.Promise;
+
+    mongoose.connection.on('connected', () => {
+        console.log('Mongoose has successfully connected!');
+    });
+
+    mongoose.connection.on('err', err => {
+        console.error(`Mongoose connection error: \n${err.stack}`);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.warn('Mongoose connection lost');
+    });
+
+
+};
