6c5e274c6fdb5ecb199effee9a3ddf643d0fec94
Gary <uncleneedham@gmail.com>
2020-11-28 22:14:04 -0600

    updates

--- src/Commands/Ping.js
+++ src/Commands/Ping.js
@@ -4,7 +4,7 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			aliases: ['pong'],
+			aliases: [],
 			description: 'This provides the ping of the bot',
 			category: 'Utilities'
 		});
